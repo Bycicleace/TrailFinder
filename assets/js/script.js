a8a328b97c128900727d9b9722c51151be915281
Adam Tilly <tillyjadam@gmail.com>
2021-09-30 19:37:46 -0500

    added function call with the state imput to the first api call.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,24 @@
-console.log("hello");
 
+//Get input from search box
 document.getElementById("button").addEventListener("click", function(){
-    var city = document.getElementById("city-entry").value; 
-    console.log(city);
-}); 
+    var state = document.getElementById("state-entry").value; 
+    console.log(state);
+    firstAPICall(state);
+});
+
+// Use Imput to call first API
+var firstAPICall =function(stateSearch){
+// fetch(https://developer.nps.gov/api/v1/parks?api_key=CV0ig8nWQLFF65A4f4FNghhUov7ovwklkr4ybJ6E)
+  initMap(lon,lat)
+}
+
+
 
 let map;
 
-function initMap() {
+function initMap(lon,lat) {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: lat, lng: lon },
     zoom: 8,
   });
 }
